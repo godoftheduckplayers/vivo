372ee6acf8926c22e6c6afd941eb978f9253262b
Leandro Marques <lmarqueo@emeal.nttdata.com>
2024-12-04 15:42:39 -0300

    Remove unused agents and configurations.
    This commit deletes the 'information' and 'optimization mechanisms' crews, including their associated agent and task configurations. By doing so, it simplifies the codebase by removing components that are no longer required, improving maintainability. The main script has also been updated to exclude references to these obsolete components.

--- src/vivo/main.py
+++ src/vivo/main.py
@@ -3,12 +3,10 @@
 import asyncio
 
 import websockets
-from crewai.flow.flow import Flow, listen, start, router, or_
+from crewai.flow.flow import Flow, start
 from pydantic import BaseModel
 
 from src.vivo.crews.aura.aura import Aura
-from src.vivo.crews.information.information import Information
-from src.vivo.crews.optimizationmechanisms.optimizationmechanisms import OptimizationMechanisms
 
 EXIT_MESSAGES = ['exit', 'quit']
 GOODBYE_MESSAGE = "Thank you so much for contacting us! 😊 It was a pleasure helping you. If you need anything else, just reach out. I'll always be here for anything you need. 💙 See you soon!"
@@ -32,31 +30,6 @@ class AuraFlow(Flow[AuraState]):
             .kickoff(inputs={"topic": self.state.userInput})
         )
         self.state.topic = result.raw
-
-    @router(classified_user_intent)
-    def redirect_service(self):
-        if self.state.topic.__contains__("information"):
-            return "information"
-        return self.state.topic
-
-    @listen("information")
-    def process_information(self):
-        result = (
-            Information()
-            .crew()
-            .kickoff(inputs={"topic": self.state.userInput})
-        )
-        self.state.auraResponse = result.raw
-
-    @listen(or_(process_information))
-    def optimization_mechanisms(self):
-        result = (
-            OptimizationMechanisms()
-            .crew()
-            .kickoff(inputs={"topic": self.state.userInput,
-                             "output": self.state.auraResponse})
-        )
-        self.state.auraResponse = result.raw
 
 
 async def chat(websocket):
